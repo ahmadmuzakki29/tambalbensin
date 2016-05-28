e632ea303483dfdc2197932c4a8b8b73ba93d84d
Ahmad Muzakki <muzakki.ahmad29@gmail.com>
2016-05-28 19:11:35 +0700

    show route ban

--- app/src/main/java/tk/fathony/tambalbensin/MainActivity.java
+++ app/src/main/java/tk/fathony/tambalbensin/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String HOST = "http://pombensin.tk/";
     private static final String URL_POM = "pom.php";
+    private static final String URL_BAN = "ban.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
         Intent in = new Intent(this,MapsActivity.class);
         in.putExtra("url",HOST+URL_POM);
         in.putExtra("jenis","pom");
+        startActivity(in);
+    }
+
+    public void mapBan(View v){
+        Intent in = new Intent(this,MapsActivity.class);
+        in.putExtra("url",HOST+URL_BAN);
+        in.putExtra("jenis","ban");
         startActivity(in);
     }
 }
